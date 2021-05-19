d6a14b6a32d03d9aefc752d81e1b0a6c6570a39f
Vladimir Kubakh <refrain14@list.ru>
2021-05-19 17:41:27 +0300

    Apollo with class components without deprecated HOC

---
+++ apolloWithClassComponents.js
@@ -0,0 +1,68 @@
+/*Подключаем клиент и кэш для работы аполло*/
+const client = new ApolloClient({
+    uri: 'http://localhost:8000/graphql/',
+    cache: new InMemoryCache()
+  });
+
+
+/*Перечисляем основные запросы и мутации
+для работы компонента*/
+let MUTATION = gql`
+  mutation {
+    КАКАЯ-ТО МУТАЦИЯ
+  }`;
+
+
+/*HOC, в который обернём классовый компонент
+и в которой можно юзать хуки. Ее задача только
+пробросить колбэки для работы с аполло*/
+let Сonnector = function(Component, Querie){
+  return function() {
+    let [mutate] = useMutation(Querie);
+    return (
+        <Component mutate={mutate}/>
+    );
+  };    
+};
+
+
+/*Сам классовый компонент, который не может
+использовать аполловские хуки, но содержит основную
+логику*/
+class BehaviorClassComponent extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {data:""};
+    this.someHandler = this.someHandler.bind(this);
+  }
+
+  someHandler(){
+    this.props.mutate({variables:{data:this.state.data}}).then((result)=>console.log(result));
+  }
+
+  render(){
+    return (
+      <>
+      <input type="text" value={this.state.data} onChange={(e)=>{this.setState({data:e.target.value})}}/>
+      <button onClicl={this.someHandler}/>
+      </>
+    );
+  }
+}
+
+
+
+let EnhancedBehaviorClassComponent = Connector(BehaviorClassComponent, MUTATION);
+
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <EnhancedBehaviorClassComponent/>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+
+
